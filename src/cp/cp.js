f30c1e6603fabbc6d793f99ab61456c312a33fd9
Nadyushka <12nadya11@gmail.com>
2024-10-04 16:28:21 +0300

    feature: add solution to cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { fork } from 'node:child_process'
+import path from 'node:path'
+import { getFileAndFolderPath } from "../tools/getFileAndFolderPath.js";
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+
+    const folderName= 'files'
+    const fileName = 'script.js'
+    const { __dirname } = getFileAndFolderPath(import.meta.url)
+
+    const scriptPath = path.join(__dirname, folderName, fileName)
+
+    const childProcess = fork(scriptPath, args)
+
+    childProcess.on('error', (error) => {
+        console.error(`Error spawning child process: ${error.message}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1, 2, 3]);
